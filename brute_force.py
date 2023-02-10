f05a993f94c1199668955a7a8cda93d76ea4d27f
mattmetcalf059 <124944020+mattmetcalf059@users.noreply.github.com>
2023-02-10 08:56:44 -0500

    Update brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,18 +1,28 @@
-import subprocess
 import time
-import itertools
+import subprocess
 
-start = time.time()
+# Character set for password generation
+charset = 'abcdefghijklmnopqrstuvwxyz'
+password_length = 8 # Assumed password length
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-password_length = 6
+# Function to calculate the time taken for password verification
+def check_password(password):
+    start_time = time.time()
+    result = subprocess.run(['./vault.o', password], stdout=subprocess.PIPE)
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return (result.stdout.decode().strip(), time_taken)
 
-for password_tuple in itertools.product(alphabet, repeat=password_length):
-    password = "".join(password_tuple)
-    result = subprocess.run(["./vault.o", password], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if b"Success" in result.stdout:
-        print("Found password:", password)
+# Brute-force attack
+for i in range(26 ** password_length):
+    password = ''
+    temp = i
+    for j in range(password_length):
+        password = charset[temp % 26] + password
+        temp //= 26
+
+    result, time_taken = check_password(password)
+    if result == 'Success':
+        print(f'Password found: {password}')
         break
-
-end = time.time()
-print("Elapsed time:", end - start, "seconds")
+    elif time_taken >=
